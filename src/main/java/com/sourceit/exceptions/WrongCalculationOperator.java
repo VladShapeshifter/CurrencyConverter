6a9cfa8dc07a13b53939494c984be82c1b763794
Vladislav <vladislav.borsch@gmail.com>
2014-12-23 20:09:18 +0200

    simple part of big mechanism

---
+++ src/main/java/com/sourceit/exceptions/WrongCalculationOperator.java
@@ -0,0 +1,10 @@
+package com.sourceit.exceptions;
+
+/**
+ * Created by Vladislav on 20.12.2014.
+ */
+public class WrongCalculationOperator extends Exception {
+    public WrongCalculationOperator(String message) {
+        super(message);
+    }
+}
