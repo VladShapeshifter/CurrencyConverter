02d83a1bb528879f20edbc8e758c3b705efe3910
Vladislav <vladislav.borsch@gmail.com>
2014-12-31 13:11:24 +0200

    priority determination changed from switch logic to Map logic instead, somewhere char c changed to String str for add new priority to "toDollar" view

--- src/main/java/com/sourceit/calculation/ReversePolishNotation.java
+++ src/main/java/com/sourceit/calculation/ReversePolishNotation.java
@@ -6,12 +6,15 @@ import com.sourceit.models.Currency;
 import com.sourceit.operators.*;
 
 import java.io.*;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 /**
  * Created by bvl on 12/19/2014.
  */
 public class ReversePolishNotation {
+    static String toCurrency = "";
     static boolean isDelim(char c) { // тру если пробел
         return c == ' ';
     }
@@ -24,34 +27,29 @@ public class ReversePolishNotation {
     private static boolean isLetter(char c) throws IOException {
         return Character.isAlphabetic(c);
     }
-    static int priority(char op) {
-        switch (op) { // при + или - возврат 1, при * / 2 иначе -1
-            case '+':
-            case '-':
-                return 1;
-            case '*':
-            case '/':
-                return 2;
-            case 'c': // conversion - значит отслежено выражение типа "toDollar"
-                return 3;
-            default:
-                return -1;
-        }
+    static int priority(String op) {
+        Map switchMap = new HashMap();
+        switchMap.put("+", 1);
+        switchMap.put("-", 1);
+        switchMap.put("*", 2);
+        switchMap.put("/", 2);
+        switchMap.put(toCurrency, 3);
+        return ((int)switchMap.get(op));
     }
-    static void processOperator(LinkedList<Object> st, char op) throws WrongCalculationOperator {
+    static void processOperator(LinkedList<Object> st, String op) throws WrongCalculationOperator {
         Object r = st.removeLast(); // выдёргиваем из упорядоченного листа последний элемент
         Object l = st.removeLast(); // выдергиваем предпоследний элемент
         switch (op) { // выполняем действие между l и r в зависимости от оператора в кейсе и результат валим в st
-            case '+':
+            case "+":
                 st.add(calculate(r, l, new Addition()));
                 break;
-            case '-':
+            case "-":
                 st.add(calculate(r, l ,new Subtraction()));
                 break;
-            case '*':
+            case "*":
                 st.add(calculate(r, l, new Multiplication()));
                 break;
-            case '/':
+            case "/":
                 st.add(calculate(r, l, new Division()));
                 break;
         }
@@ -70,29 +68,28 @@ public class ReversePolishNotation {
 
     public static Object eval(String s) throws WrongCalculationOperator, IOException { // вводим выражение
         LinkedList<Object> st = new LinkedList<Object>(); // сюда наваливают цифры
-        LinkedList<Character> op = new LinkedList<Character>(); // сюда опрераторы, и st и op в порядке поступления
-        String toCurrency = "";
+        LinkedList<String> op = new LinkedList<String>(); // сюда опрераторы, и st и op в порядке поступления
+//        String toCurrencyIn = "";
         for (int i = 0; i < s.length(); i++) { // парсим строку с выражением и вычисляем
             char c = s.charAt(i);
+            String str = Character.toString(c);
             if (isDelim(c)) // каждый последующим символ всегда проверяется, не пробел ли он
                 continue;
             if (isLetter(c)) {
                 toCurrency += c;
-
-
             }
             else if (c == '(')
-                op.add('('); // иначе елси символ = "(", то добавляем "(" в операторы
+                op.add("("); // иначе елси символ = "(", то добавляем "(" в операторы
             else if (c == ')') {
-                while (op.getLast() != '(')
+                while (op.getLast() != "(")
                     processOperator(st, op.removeLast()); // иначе если символ = ")", то, пока последний символ в op не "(", добавляем результат сложения
                     // (+ - * /) над последним и предпоследним числами в st, одновременно передаем и удаляем последний оператор из op
                 op.removeLast(); // удалить "("
             } else if (isOperator(c)) {
-                while (!op.isEmpty() && priority(op.getLast()) >= priority(c))// иначе если символ = (+-*/), то пока в op есть
+                while (!op.isEmpty() && priority(op.getLast()) >= priority(str))// иначе если символ = (+-*/), то пока в op есть
                 // операторы, выполнять действия надчислами согласно приоритету действия, одновременно передаем и удаляем последний оператор из op
                     processOperator(st, op.removeLast());
-                op.add(c); // добавить +-*/
+                op.add(str); // добавить +-*/
             } else { // иначе, т.е. если это цифры или буквы или $, то добавить их в st
                 String value = "";
                 String type = "";
